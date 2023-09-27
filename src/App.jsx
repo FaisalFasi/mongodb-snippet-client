ead2963abdbdb3ef764d2f52cb00552f0a1a9a27
faisalrehman <faisalfasi18@gmail.com>
2023-09-27 15:01:48 +0200

    removed icon from html link

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, useParams } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import { ToastContainer } from "react-toastify";
 
